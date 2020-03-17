98f1d71c40e640db6e4d6d7aaaa1898d7434a68f
Sergei <57198066+YourDaddy5@users.noreply.github.com>
2020-03-17 23:32:54 +0300

    Add files via upload

---
+++ something.py
@@ -0,0 +1,28 @@
+from collections import Counter
+stuff = {'coin':42,'torch':2,'helmet':1,'magic egge':1}
+def DisplayInventory(inventory):
+    print('Inventory: ')
+    for k,v in stuff.items(): print(k, v)
+
+    return ''
+
+
+loot = ['coin','dragons head','coin','d','coin','a','coin']
+c  = Counter(loot)
+def add(inv,added):
+    for l in added:
+        if l in inv.keys():
+            inv[l] = inv[l] + c[l]
+            break
+        else:
+            pass
+            #ВОТ ТУТ НАДО ДОБАВИТЬ ДРУГИЕ ЭЛЕМЕНТЫ КОТОРЫХ НЕТ
+
+
+
+    print(inv)
+
+
+
+
+print(add(stuff,loot))
